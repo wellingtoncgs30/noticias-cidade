544f1e655cfe9c0b05ecdcbe55f33b5d2eef4ffa
wellingtoncgs30 <wellingtoncgs30@gmail.com>
2021-01-02 22:47:24 -0300

    Corrigindo erro de import do link do nextjs

--- pages/saude/brasil-inicia-vacinacao-contra-o-novo-coronavirus.js
+++ pages/saude/brasil-inicia-vacinacao-contra-o-novo-coronavirus.js
@@ -1,4 +1,4 @@
-import Link from "next/Link"
+import Link from "next/link"
 import graphql from "graphql-tag"
 import client from "../../components/ApolloClient"
 import Layout from "../../components/Layout"
